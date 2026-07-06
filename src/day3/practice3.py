fa90374a3b05eb53675ce43014b4f098f8c6d9ce
Yashanth <yashanth.m@diggibyte.com>
2026-07-06 12:25:12 +0530

    add day5 prac

--- src/day3/practice3.py
+++ src/day3/practice3.py
@@ -1,4 +1,44 @@
-#
+#sets
+
+s = {10, 20, 30, 40}
+
+# Add one element
+s.add(50)
+
+# Add multiple elements
+s.update([60, 70])
+
+# Remove an element
+s.remove(20)
+
+# Membership
+if 30 in s:
+    print("30 Found")
+
+# Iterate
+print("Elements:")
+for i in s:
+    print(i)
+
+# Length
+print("Total Elements:", len(s))
+
+#tuples
+t = (10, 20, 30, 40, 20)
+
+print("First Element:", t[0])
+print("Slice:", t[1:4])
+
+print("Elements:")
+for i in t:
+    print(i)
+
+print("Count of 20:", t.count(20))
+print("Index of 30:", t.index(30))
+print("Length:", len(t))
+
+if 40 in t:
+    print("40 Found")
 
 #dictionaries
 
